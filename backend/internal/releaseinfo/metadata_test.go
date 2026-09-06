346eb0bc62740144bd1657259bce87bc2787ee80
Ryan <73210077+firedvl@users.noreply.github.com>
2026-09-06 07:42:05 -0700

    chore(release): prepare v0.2.0-rework.8

--- backend/internal/releaseinfo/metadata_test.go
+++ backend/internal/releaseinfo/metadata_test.go
@@ -11,7 +11,7 @@ import (
 
 func TestEmbeddedMetadataIsCanonicalAndManual(t *testing.T) {
 	metadata := Current()
-	require.Equal(t, "0.2.0-rework.7", metadata.ReworkVersion)
+	require.Equal(t, "0.2.0-rework.8", metadata.ReworkVersion)
 	require.Equal(t, "v0.2.0", metadata.UpstreamBaseline)
 	require.Equal(t, "aa236488351eb71e120fc2b6fb32e36b0374c918", metadata.UpstreamBaselineSHA)
 	require.Equal(t, "ghcr.io/firedvl/sub2api-rework", metadata.ArtifactRepository)
